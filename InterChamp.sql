fad4c744e2bd24b3241cd4b48a1ca8d82d470e0e
Juan Pablo <jpschiaffino@gmail.com>
2022-06-06 12:28:19 -0400

    competitor with foreign keys is ready

--- InterChamp.sql
+++ InterChamp.sql
@@ -1,23 +1,35 @@
-CREATE DATABASE "InterChamp"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1;
+/* CREATE DATABASE InterChamp */
+/*     WITH */
+/*     OWNER = postgres */
+/*     ENCODING = 'UTF8' */
+/*     CONNECTION LIMIT = -1; */
 
-    CREATE TABLE public.Competitor(
-        rut integer NOT NULL,
-        first_name name NOT NULL,
-        second_name name,
-        last_name name NOT NULL,
-        last_second_name name,
-        role1 text NOT NULL,
-        gender text NOT NULL,
-        age integer NOT NULL,
-        id_team integer NOT NULL,
-        id_school integer NOT NULL,
-        CONSTRAINT rut_pkey PRIMARY KEY (rut)
-    );
+DROP TABLE IF EXISTS Competitor CASCADE;
+DROP TABLE IF EXISTS Team CASCADE;
 
-    ALTER TABLE IF EXISTS public.Competitor
-        OWNER to postgres;
+CREATE TABLE public.Competitor(
+    rut integer NOT NULL,
+    first_name name NOT NULL,
+    second_name name,
+    last_name name NOT NULL,
+    last_second_name name,
+    id_role1 text NOT NULL,
+    gender text NOT NULL,
+    age integer NOT NULL,
+    id_team integer NOT NULL,
+    id_school integer NOT NULL,
+    CONSTRAINT rut_pk PRIMARY KEY (rut)
+    /* CONSTRAINT fk_id_team FOREIGN KEY (id_team) */
+    /*     REFERENCES public.Team (id_team), */
+    /* CONSTRAINT fk_id_role1 FOREIGN KEY (id_role1) */
+    /*     REFERENCES public.Role1 (id_role1), */
+    /* CONSTRAINT fk_id_school FOREIGN KEY (id_school) */
+);
+
+ALTER TABLE IF EXISTS public.Competitor
+    OWNER to postgres;
+
+CREATE TABLE public.Team(
+
+);
 
