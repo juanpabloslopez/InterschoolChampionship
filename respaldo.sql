71fff2f27d6dd9f285d1eb58c67e2a9fef26167a
Juan Pablo <jpschiaffino@gmail.com>
2022-06-26 22:27:59 -0400

    respaldo created

--- respaldo.sql
+++ respaldo.sql
@@ -1,34 +1,42 @@
-DROP DATABASE IF EXISTS InterChamp;
-CREATE DATABASE InterChamp
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1;
+/* DROP DATABASE IF EXISTS interchamp; */
+/* CREATE DATABASE interchamp */
+/*     WITH */
+/*     OWNER = postgres */
+/*     ENCODING = 'UTF8' */
+/*     CONNECTION LIMIT = -1; */
+
+/* DROP TABLE IF EXISTS Match CASCADE; */
+/* DROP TABLE IF EXISTS Competitor CASCADE; */
+/* DROP TABLE IF EXISTS Court CASCADE; */
+/* DROP TABLE IF EXISTS Team CASCADE; */
+/* DROP TABLE IF EXISTS School CASCADE; */
+/* DROP TABLE IF EXISTS Role1 CASCADE; */
+/* DROP TABLE IF EXISTS Discipline CASCADE; */
 
 CREATE TABLE Discipline(
     id_discipline integer NOT NULL,
     name_discipline name NOT NULL,
-    CONSTRAINT id_discipline_pk PRIMARY KEY (id_discipline)
+    PRIMARY KEY (id_discipline)
 );
 
 CREATE TABLE Role1(
     id_role1 integer NOT NULL,
     name_role1 name NOT NULL,
-    CONSTRAINT id_role1_pk PRIMARY KEY (id_role1)
+    PRIMARY KEY (id_role1)
 );
 
 CREATE TABLE School(
     id_school integer NOT NULL,
     name_school name NOT NULL,
-    CONSTRAINT id_school_pk PRIMARY KEY (id_school)
+    PRIMARY KEY (id_school)
 );
 
 CREATE TABLE Team(
     id_team integer NOT NULL,
     id_school integer NOT NULL,
     team_name name NOT NULL,
-    CONSTRAINT id_team_pk PRIMARY KEY (id_team),
-    CONSTRAINT fk_id_school FOREIGN KEY (id_school)
+    PRIMARY KEY (id_team),
+    FOREIGN KEY (id_school)
         REFERENCES School (id_school)
 );
 
@@ -36,8 +44,8 @@ CREATE TABLE Court(
     id_court integer NOT NULL,
     id_school integer NOT NULL,
     name_court name NOT NULL,
-    CONSTRAINT id_court_pk PRIMARY KEY (id_court),
-    CONSTRAINT fk_id_school FOREIGN KEY (id_school)
+    PRIMARY KEY (id_court),
+    FOREIGN KEY (id_school)
         REFERENCES School (id_school)
 );
 
@@ -52,73 +60,106 @@ CREATE TABLE Competitor(
     age integer NOT NULL,
     id_team integer NOT NULL,
     id_school integer NOT NULL,
-    CONSTRAINT rut_pk PRIMARY KEY (rut),
-    CONSTRAINT fk_id_team FOREIGN KEY (id_team)
+    PRIMARY KEY (rut),
+    FOREIGN KEY (id_team)
         REFERENCES Team (id_team),
-    CONSTRAINT fk_id_role1 FOREIGN KEY (id_role1)
+    FOREIGN KEY (id_role1)
         REFERENCES Role1 (id_role1),
-    CONSTRAINT fk_id_school FOREIGN KEY (id_school)
+    FOREIGN KEY (id_school)
         REFERENCES School (id_school)
 );
 
 CREATE TABLE Match(
     id_match integer NOT NULL,
     score_team1 integer NOT NULL,
-    score_team2 integer NOT NULL,
+    score_team2 integer NOT NULL, 
     id_team1 integer NOT NULL,
     id_team2 integer NOT NULL,
     id_discipline integer NOT NULL,
     id_court integer NOT NULL,
     date date NOT NULL,
-    CONSTRAINT id_match_pk PRIMARY KEY (id_match),
-    CONSTRAINT fk_id_team1 FOREIGN KEY (id_team1)
+    PRIMARY KEY (id_match),
+    FOREIGN KEY (id_team1)
         REFERENCES Team (id_team),
-    CONSTRAINT fk_id_team2 FOREIGN KEY (id_team2)
+    FOREIGN KEY (id_team2)
         REFERENCES Team (id_team),
-    CONSTRAINT fk_id_discipline FOREIGN KEY (id_discipline)
+    FOREIGN KEY (id_discipline)
         REFERENCES Discipline (id_discipline),
-    CONSTRAINT fk_id_court FOREIGN KEY (id_court)
+    FOREIGN KEY (id_court)
         REFERENCES Court (id_court)
 );
 
-/* INSERT INTO public.Comuna( */
-/*     nombre_com, id_com) */
-/*     VALUES ('Providencia', 7500000); */
+/* [Discipline */
 
-/* Referencias para agregar competidores */
+INSERT INTO Discipline(
+    id_discipline, name_discipline)
+    VALUES(1, 'Futbol');
 
-/* [Role1] */
-/* id_role1 = 1 (Arquero) */
-/* id_role1 = 9 (Goleador) */
-/* id_role1 = 10 (Creador) */
+INSERT INTO Discipline(
+    id_discipline, name_discipline)
+    VALUES(2, 'Basketball');
 
-/* [Team] */
-/* id_team = 1 ('Coca Juniors') */
-/* id_team = 2 ('Vokda Juniors') */
-/* id_team = 3 ('Union Penosa') */
-/* id_team = 4 ('Indio cu') */
-/* id_team = 5 ('Real Alcoholicos') */
+INSERT INTO Discipline(
+    id_discipline, name_discipline)
+    VALUES(3, 'Volleyball');
 
-/* [School] */
-/* id_school = 1 ('Liceo Lastarria') */
-/* id_school = 2 ('Nacional') */
-/* id_school = 3 ('Liceo 7') */
+INSERT INTO Discipline(
+    id_discipline, name_discipline)
+    VALUES(4, 'PingPong');
 
-/* [Competitor */
+INSERT INTO Discipline(
+    id_discipline, name_discipline)
+    VALUES(5, 'HandBall');
 
-INSERT INTO Competitor(
-    rut, first_name, second_name, last_name, last_second_name, id_role1, gender, age, id_team, id_school)
-    VALUES(209570653, 'Jozsef', 'Jesus', 'Reyes', 'Bascones', 3, 'Male', 20, 1, 1);
+INSERT INTO Discipline(
+    id_discipline, name_discipline)
+    VALUES(6, 'Rugby');
 
-/* INSERT INTO Competitor( */
-/*     rut, first_name, second_name, last_name, last_second_name, id_role1, gender, age, id_team, id_school) */
-/*     VALUES(97018359, 'Juan', 'Jose', 'Duarte', 'Lange', 9, 'Male', 50, 5, 2); */
+INSERT INTO Discipline(
+    id_discipline, name_discipline)
+    VALUES(7, 'Taca-Taca');
 
-/* INSERT INTO Competitor( */
-/*     rut, first_name, second_name, last_name, last_second_name, id_role1, gender, age, id_team, id_school) */
-/*     VALUES(197945591, 'Leonardo', 'Andres', 'Rojas', 'Rebolledo', 10, 'Male', 23, 4, 3); */
+INSERT INTO Discipline(
+    id_discipline, name_discipline)
+    VALUES(8, 'Ajedrez');
 
-/* Competitor] */
+/* Discipline] */
+
+/* [Role1 */
+
+INSERT INTO Role1(
+    id_role1, name_role1)
+    VALUES(1, 'Arquero');
+
+INSERT INTO Role1(
+    id_role1, name_role1)
+    VALUES(5, 'Pivote');
+
+INSERT INTO Role1(
+    id_role1, name_role1)
+    VALUES(9, 'Delantero');
+
+INSERT INTO Role1(
+    id_role1, name_role1)
+    VALUES(10, 'Creador');
+
+/* Role1] */
+
+/* [School */
+
+INSERT INTO School(
+    id_school, name_school)
+    VALUES(1, 'Liceo Lastarria');
+
+INSERT INTO School(
+    id_school, name_school)
+    VALUES(2, 'Liceo 7');
+
+INSERT INTO School(
+    id_school, name_school)
+    VALUES(3, 'Nacional');
+
+/* School] */
 
 /* [Team */
 
@@ -126,20 +167,60 @@ INSERT INTO Team(
     id_team, id_school, team_name)
     VALUES(1, 1, 'Coca Juniors');
 
+INSERT INTO Team(
+    id_team, id_school, team_name)
+    VALUES(2, 1, 'Union Penosa');
+
+INSERT INTO Team(
+    id_team, id_school, team_name)
+    VALUES(3, 2, 'Real Alcoholicos');
+
+INSERT INTO Team(
+    id_team, id_school, team_name)
+    VALUES(4, 2, 'Indio-Cu');
+
+INSERT INTO Team(
+    id_team, id_school, team_name)
+    VALUES(5, 3, 'Vokda Juniors');
+
 /* Team] */
 
+/* [Court */
 
+INSERT INTO Court(
+    id_court, id_school, name_court)
+    VALUES(1, 1, 'Monumental');
 
+INSERT INTO Court(
+    id_court, id_school, name_court)
+    VALUES(2, 2, 'San Carlos de Apoquindo');
 
+INSERT INTO Court(
+    id_court, id_school, name_court)
+    VALUES(3, 3, 'FakeEstadiodelaU');
 
+/* Court] */
 
+/* [Competitor */
 
+INSERT INTO Competitor(
+    rut, first_name, second_name, last_name, last_second_name, id_role1, gender, age, id_team, id_school)
+    VALUES(209570653, 'Jozsef', 'Jesus', 'Reyes', 'Bascones', 1, 'Male', 20, 1, 1);
 
+INSERT INTO Competitor(
+    rut, first_name, second_name, last_name, last_second_name, id_role1, gender, age, id_team, id_school)
+    VALUES(97018359, 'Juan', 'Jose', 'Duarte', 'Lange', 9, 'Male', 50, 2, 2);
 
+INSERT INTO Competitor(
+    rut, first_name, second_name, last_name, last_second_name, id_role1, gender, age, id_team, id_school)
+    VALUES(197945591, 'Leonardo', 'Andres', 'Rojas', 'Rebolledo', 10, 'Male', 23, 3, 3);
 
+/* Competitor] */
 
+/* [Match */
 
-/* ARP DEBERIA ENTRAR */
-/* EMPRESA PING LATENCIA */
-/* PROGRAMAS QUE USO */
-/* traceroute */
+INSERT INTO Match(
+    id_match, score_team1, score_team2, id_team1, id_team2, id_discipline, id_court, date)
+    VALUES(1, 3, 2, 1, 2, 1, 1, '25/6/2022');
+
+/* Match] */
